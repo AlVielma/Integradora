9a14f05e13d1f326c3b2528f54e76d845a9cb724
AlVielma <vielma7220@gmail.com>
2023-07-18 15:21:13 -0600

    metodoscitaa

---
+++ src/modelos/metodoscita.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Modelos;
+use App\Modelos\Conexion;
+require __DIR__.'/../../vendor/autoload.php';
+
+class metodoscita
+{
+    private $connect;
+    private $conectar;
+    public function __construct()
+    {
+        $this->connect= new Conexion();
+        $this->conectar=$this->connect->conectar();
+       
+    }
+    public function mostrar()
+    {
+        $consulta = $this->conectar->query("SELECT u.nombre, cc.nombre, cc.telefono, cc.fecha_nacimiento
+        ,cc.dia,cc.hora,cc.sintomas_oculares,cc.enfermedades_oculares,cc.lentes_actualmente,cc.armazon,cc.contacto,
+        cc.ultimo_examen,cc.uso_gotas FROM Citas_Cliente cc inner join Usuarios u on u.id = cc.usuario");
+        return $consulta->fetchAll(\PDO::FETCH_ASSOC);
+    }
+    public function agregar($nombre,$telefono,$fecha_nacimiento,$dia,$hora,$sintomas_oculares,$enfermedades_oculares,$lentes_actualmente,$armazon,$contacto,$ultimo_examen
+    ,$uso_gotas)
+    {
+        $query = $this->conectar->prepare("INSERT INTO Citas_Cliente (nombre, telefono, fecha_nacimiento, dia, hora, 
+        sintomas_oculares, enfermedades_oculares, lentes_actualmente, armazon, contacto, ultimo_examen, uso_gotas) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
+        $query->execute([$nombre,$telefono,$fecha_nacimiento,$dia,$hora,$sintomas_oculares,$enfermedades_oculares,$lentes_actualmente,$armazon,$contacto,$ultimo_examen
+        ,$uso_gotas]);
+    }
+
+    
+    public function eliminar($id)
+    {
+        $query=$this->conectar->prepare("DELETE FROM Citas_Cliente WHERE id=?");
+        $query->execute([$id]);
+    }
+    public function __destruct()
+    {
+        $this->conectar = null; // Cierra la conexión establecida
+    }
+
+}
