2363a432e8f19b34ef1a58ee9c8366cd2cd3c59b
Arturo <hernandez020610@gmail.com>
2023-07-18 14:16:04 -0600

    cambios

--- admin/app/agenda.php
+++ admin/app/agenda.php
@@ -38,6 +38,13 @@
               <th>Hora</th>
             </tr>
           </thead>
+          <tbody>
+            <tr>
+              <td>
+                <!-- PHP -->
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
   </div>
