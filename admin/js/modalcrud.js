a70f4968fa1351b0f1c8cc9490da8aca887f9f10
FERNANDO <fgolmos10@gmail.com>
2023-07-18 20:52:21 -0600

    aggimg modalcrud eliminarproducto guardarproducto modaleliminarproducto modalproducto productos

---
+++ admin/js/modalcrud.js
@@ -0,0 +1,12 @@
+let eliminarmodal = document.getElementById('modaleliminarproducto');
+
+eliminarmodal.addEventListener('shown.bs.modal', event => {
+    let button = event.relatedTarget;
+    let id = button.getAttribute('data-bs-id');
+    eliminarmodal.querySelector('.modal-footer #id').value = id;
+
+    let btnAceptar = eliminarmodal.querySelector('.modal-footer button[name="eliminar"]');
+    btnAceptar.addEventListener('click', function () {
+        document.getElementById('eliminarForm').submit();
+    });
+});
--- src/http/guardarproducto.php
+++ src/http/guardarproducto.php
@@ -1,4 +1,30 @@
 <?php
+use App\Modelos\productos;
+require __DIR__.'/../../vendor/autoload.php';
 
+if(isset($_POST['agregar']))
+{
+    $productos = new productos();
+    extract($_POST);
+    extract($_FILES);
+    $dir = __DIR__.'/../../productosimg/';
+    $pathinfo = pathinfo($imagen['name']);
+    $filename = $pathinfo["filename"];
+    $extension = $pathinfo["extension"];
+    $name= "{$filename}.{$extension}";
+    $real_path = "{$dir}{$filename}.{$extension}";
+    
+    if(!file_exists($real_path))
+    {
+        move_uploaded_file($imagen["tmp_name"],$real_path);
+        $productos->agregar_imagen($name);
+        $imagenid = $productos->get_lastid();
+        $productos->agregar_producto($nombre,$marca,$tipo_lente,$descripcion,$imagenid,$precio,$stock,$categoria);
+        header('Location: /../../admin/app/aggimg.php');
+    }
+    else{
+        echo 'Archivo existente';
+    }
+}
 
 ?>
