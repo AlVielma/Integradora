1adfb9d7d3d5b78d9d6425a41a0cf20985d22fc7
ANGELES <anie05rv7@gmail.com>
2023-07-26 15:50:51 -0600

    soias

--- admin/app/consultapdf.php
+++ admin/app/consultapdf.php
@@ -1,5 +1,5 @@
 <?php
-require 'PDFGenerator.php';
+require_once 'PDFGenerator.php';
 use App\Modelos\ValidacionesConsultas;
 require_once __DIR__.'/../../src/modelos/ValidacionesConsultas.php';
 $pdfGenerator = new PDFGenerator();
