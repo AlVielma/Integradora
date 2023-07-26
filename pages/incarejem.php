0d5300b1dd81a22ba5930ea9194577c4ead71d75
FERNANDO <fgolmos10@gmail.com>
2023-07-26 15:20:50 -0600

    dasd

--- pages/incarejem.php
+++ pages/incarejem.php
@@ -127,55 +127,9 @@ session_start();
       </div>
 
     <!--footer-->
-    <div class="container-fluid border border-black footer bg-dark text-white">
-
-        <!--Footer superio-->
-        <div class="row p-5 text-aling-center">
-
-            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3">
-                <h3>Pop Ópticos</h3>
-                <a href="index.html"><img src="images/icon64.png" alt=""></a>
-
-            </div>
-            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3">
-                <p class="h5">Dirección</p>
-                <div class="mb-2">
-                    <p>Av.Juárez 4880 y Xochimilco Oriente, Torreón, Méxcio</p>
-                </div>
-            </div>
-
-            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3">
-                <p class="h5">Contacto</p>
-                <div class="mb-2">
-                    <p>871 735 8778</p>
-                </div>
-                <div class="mb-2">
-                    <a class="text-decoration-none text-white" href="#">ventas@opticapop.com</a>
-                </div>
-            </div>
-            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3">
-                <p class="h5">Redes</p>
-                <div class="mb-2">
-                    <a href=""><img src="../images/facebook.png" alt=""></a>
-                </div>
-                <div class="mb-0">
-                    <p>Facebook</p>
-                </div>
-                <div class="mb-2">
-                    <a href=""><img src="../images/whatsapp.png" alt=""></a>
-                </div>
-                <div class="mb-0">
-                    <p>Whatsapp</p>
-                </div>
-            </div>
-            <!--Derechos de autor-->
-            <div class="col-xs-12 pt-5">
-                <p class="text-white text-center"> Copyright - All rights reserved © 2023</p>
-            </div>
-
-        </div>
-
-    </div>
+    <?php
+           include 'footer.php';
+           ?>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
