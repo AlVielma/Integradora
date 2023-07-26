5b16e519e3def90229f0c0124541bd65ba5a8240
dabidgmz <david.gmzherrera28@gmail.com>
2023-07-26 14:28:03 -0600

    sanitizacion

--- src/modelos/trabajador.php
+++ src/modelos/trabajador.php
@@ -19,9 +19,23 @@ class Trabajador
         return $consulta->fetchAll(\PDO::FETCH_ASSOC);
         
     }
+    private function sanitizar($parametro){
+    $parametro = htmlspecialchars($parametro, ENT_QUOTES, 'UTF-8');
+    $parametro = filter_var($parametro, FILTER_SANITIZE_ENCODED, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH | FILTER_FLAG_ENCODE_LOW | FILTER_FLAG_ENCODE_HIGH);
+    $parametro = trim($parametro);
+    $parametro = filter_var($parametro, FILTER_SANITIZE_SPECIAL_CHARS, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH | FILTER_FLAG_ENCODE_HIGH);
+    $parametro = str_replace(array(';', '--', '*', '%', '!', '=', '<', '>'), '', $parametro);
+    if (preg_match('/<.*>|SELECT|UPDATE|DELETE|INSERT|CREATE|DROP|ALTER/i', $parametro)) {
+        return false;
+    }
+    $parametro = filter_var($parametro, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION | FILTER_FLAG_ALLOW_THOUSAND | FILTER_FLAG_ALLOW_SCIENTIFIC);
+    return $parametro;}
     public function agregar($nombre, $apellido, $email, $contraseña, $id_rol)
-    {
-        // Primero, insertamos los datos en la tabla "usuarios" sin el id_roll
+    { $nombre = $this->sanitizar($nombre);
+        $apellido = $this->sanitizar($apellido);
+        $email = $this->sanitizar($email);
+        $contraseña = $this->sanitizar($contraseña);
+        $id_rol = $this->sanitizar($id_rol);
         $query = $this->conectar->prepare("INSERT INTO Usuarios (nombre, apellido, email, contraseña, id_rol) VALUES (?, ?, ?, ?, ?)");
         $query->execute([$nombre, $apellido, $email, $contraseña, $id_rol]);
     }
