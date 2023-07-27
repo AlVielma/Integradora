abb67ed523c026209d6b967c3087f4882817f97f
AlVielma <vielma7220@gmail.com>
2023-07-27 14:14:10 -0600

    antes de la morision

--- pages/eliminar_producto.php
+++ pages/eliminar_producto.php
@@ -1,17 +1,32 @@
 <?php
 session_start();
 
-if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["sku"])) {
-    $sku = $_POST["sku"];
+require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__. '/../src/modelos/Carrito.php';
 
-    // Verificar si el producto existe en el carrito
-    if (isset($_SESSION["carrito"][$sku])) {
-        // Eliminar el producto del carrito
-        unset($_SESSION["carrito"][$sku]);
-    }
+use App\Modelos\Carrito;
+
+$carrito = new Carrito();
+
+// Verificar si el usuario ha iniciado sesión
+if(isset($_SESSION['user_id'])){
+    // Obtener el ID del usuario actual desde la sesión
+    $usuario_id = $_SESSION['user_id'];
+
+    // Obtener el SKU del producto a eliminar desde la solicitud POST
+    $sku = $_POST['sku'];
+
+    // Eliminar el producto del carrito
+    $carrito->eliminarProducto($usuario_id,$sku);
+
+    // Redirigir al usuario de vuelta al carrito
+    header('Location: prodencar.php');
+    exit();
+
+} else {
+    // El usuario no ha iniciado sesión
+    // Redirigir al usuario a la página de inicio de sesión
+    header('Location: login.php');
+    exit();
 }
-
-// Redireccionar al carrito después de eliminar el producto
-header("Location: prodencar.php");
-exit;
 ?>
