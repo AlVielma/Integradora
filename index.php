a9d273f13a417f387826562c72d3945e895b6934
ANGELES <anie05rv7@gmail.com>
2023-07-31 17:49:45 -0600

    se modifico el requerimiento del index

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 session_start();
 
 use App\Modelos\productos;
+use App\http\correoquejas;
+require_once 'src/http/correoquejas.php';
 require_once 'src/modelos/productos.php';
 require 'vendor/autoload.php';
 $productos = new productos();
