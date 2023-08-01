7a3aff735179e5a7dc3c92031252aa1c2d686a0a
dabidgmz <david.gmzherrera28@gmail.com>
2023-07-31 19:17:41 -0600

    Merge branch 'mi-nueva-rama' of https://github.com/AlVielma/Integradora into mi-nueva-rama

--- src/http/correoquejas.php
+++ src/http/correoquejas.php
@@ -1,44 +1,44 @@
 <?php
-/*correoquejas.php*/
+// correoquejas.php
+
 // Importa las clases de PHPMailer
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 // Carga el autoloader de Composer
-require __DIR__ . '../../vendor/autoload.php';
+require __DIR__ . '/../../vendor/autoload.php';
 
+// Verifica si el formulario fue enviado antes de procesar los datos
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $comentario = $_POST["comentario"];
 
-// Crea una nueva instancia de PHPMailer
-$mail = new PHPMailer(true);
+    // Crea una nueva instancia de PHPMailer
+    $mail = new PHPMailer(true);
 
-try {
-    // Configura los ajustes del servidor SMTP
-    extract($_POST);
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Habilita la salida detallada del servidor SMTP
-    $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'david.gmzherrera28@gmail.com';
-    $mail->Password   = 'nycjlrjcyoskhqfp';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port       =  587;
+    try {
+        // Configura los ajustes del servidor SMTP
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER; // Habilita la salida detallada del servidor SMTP
+        $mail->isSMTP();
+        // ...
 
-    // Configura los destinatarios
-    $mail->setFrom($email, $nombre . ' ' . $apellido);
-    $mail->addAddress('vafd_utt1@gmail.com');
-    $mail->addReplyTo($email, $nombre . ' ' . $apellido);
+        // Configura los destinatarios
+        $mail->setFrom($email, $nombre);
+        // ...
 
-    // Configura el contenido del correo
-    $mail->isHTML(true);
-    $mail->Subject = 'Esto es una prueba';
-    $mail->Body    = 'Hola, ' . $nombre . ' ' . $apellido . '!<br><br>.' . $asunto;
+        // Configura el contenido del correo
+        // ...
 
-    // Envía el correo
-    $mail->send();
-    // Redirecciona a la página principal y muestra una alerta
-    echo '<script>alert("El correo ha sido enviado correctamente");</script>';
-    echo '<script>window.location.href = "../index.php";</script>';
-} catch (Exception $e) {
-    echo '<script>alert("No se pudo enviar el correo. Error del correo: ' . $mail->ErrorInfo . '");</script>';
+        // Envía el correo
+        $mail->send();
+
+        // Redirecciona a la página principal y muestra una alerta
+        echo '<script>alert("El correo ha sido enviado correctamente");</script>';
+        echo '<script>window.location.href = "index.php";</script>';
+        exit(); // Asegura que no se ejecuten más líneas de código después de la redirección
+    } catch (Exception $e) {
+        echo '<script>alert("No se pudo enviar el correo. Error del correo: ' . $mail->ErrorInfo . '");</script>';
+    }
 }
