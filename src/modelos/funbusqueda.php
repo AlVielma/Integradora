4f79c0f03e8dfe0c54467da12dc803d3f5766fb3
Arturo <hernandez020610@gmail.com>
2023-08-06 18:59:36 -0600

    .

--- src/modelos/funbusqueda.php
+++ src/modelos/funbusqueda.php
@@ -1,24 +1,10 @@
 <?php
 
 namespace App\Modelos;
-require_once 'Conexion.php';
-use App\Modelos\Conexion;
+
 require __DIR__.'/../../vendor/autoload.php';
 
 class busque{
-    private $conexion;
-    private $pdo;
-
-    public function __construct() 
-    {
-        $this->conexion = new Conexion();
-        $this->pdo = $this->conexion->conectar();
-    }
-
-    public function buscar()
-    {
-
-    }
 
     public function ordenar($orden, $pop)
     {
