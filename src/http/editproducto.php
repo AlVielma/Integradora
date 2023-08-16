8a9c5f496fad5672e212622e772b9f96c90d5ff7
Arturo <hernandez020610@gmail.com>
2023-08-15 18:48:50 -0600

    .

--- src/http/editproducto.php
+++ src/http/editproducto.php
@@ -192,7 +192,7 @@ if(isset($_POST['editar']))
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <script>
-       
+        history.replaceState(null, null, location.pathname);
     </script>
   </body>
 </html>
